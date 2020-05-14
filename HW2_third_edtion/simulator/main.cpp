edc6f8b7594685807491d09f308f6767fd0a5955
omarMokh <62391170+omarMokh@users.noreply.github.com>
2020-05-14 15:01:41 +0300

    first commit

---
+++ HW2_third_edtion/simulator/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "Simulator.h"
+#include <memory>
+#include <dlfcn.h>
+#include "../interfaces/AbstractAlgorithm.h"
+using namespace std;
+
+struct DLCloser
+{
+	void operator()(void *dlhandle) const noexcept{
+	std::cout<<"cosint";
+	dlclose(dlhandle);
+	}
+};
+int main()
+{
+	std::shared_ptr<AbstractAlgorithm> handle1(dlopen("../algorithm/_007_a.so",RTLD_LAZY));
+	if(!handle1)
+		std::cout<<"open faild"<<endl;
+	else{
+		std::cout<<"open 007 okey"<<endl;
+		}
+	vector<shared_ptr<AbstractAlgorithm>> algo;
+	//shared_ptr<AbstractAlgorithm> a=make_shared<Algorithm>();
+	//shared_ptr<AbstractAlgorithm> b=make_shared<Algorithm>();
+	algo.push_back(handle1);
+	//algo.push_back(b);
+	string inPath = "/home/omarabomokh/eclipseWorkspaceCPP/Hw2_2edtion/simolator2";
+	string outPath = "/home/omarabomokh/eclipseWorkspaceCPP/Hw2_2edtion";
+	Simulator S(inPath,outPath,algo);
+
+}
